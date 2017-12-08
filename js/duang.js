3357fc0dc2cf3cdbe7d47367620f5a9a7d19517c
luoming <18618321192@163.com>
2017-12-08 15:53:53 +0800

    success

--- js/duang.js
+++ js/duang.js
@@ -1,17 +1,18 @@
 $(function() {
-    $('svg[data-src]').each(function(index, svg) {
+   $('svg[data-src]').each(function(index, svg){
         var src = $(svg).data('src');
-        $.ajax({
-            url: src,
-            dataType: 'xml',
-            success: function(content) {
-                var doc = content.documentElement;
-                $(doc).attr({
-                    width: $(svg).attr('width'),
-                    height: $(svg).attr('height')
-                });
-                $(svg).after(doc).remove();
-            }
-        })
-    });
+
+       $.ajax({
+           url : src,
+           dataType : 'xml',
+           success:function(svgDocument){
+               var $svg = $(svgDocument.documentElement);
+               $svg.attr({
+                   'width' : $('svg').attr('width'),
+                   'height': $('svg').attr('height')
+               })
+                $(svg).after($svg).remove();
+           }
+       })
+   });
 });
